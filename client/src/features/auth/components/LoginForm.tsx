17d135c4acc13fdc658e65e46442392c1a91b088
jeffyevangelista <jevangelista@classify.com.ph>
2025-10-28 09:54:54 +0800

    feat: implement login page with carousel and Microsoft SSO integration

--- client/src/features/auth/components/LoginForm.tsx
+++ client/src/features/auth/components/LoginForm.tsx
@@ -1,90 +1,99 @@
-import { useState } from "react";
-import { useLogin } from "../auth.hooks";
-
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
   Field,
-  FieldDescription,
   FieldGroup,
   FieldLabel,
+  FieldSeparator,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import MSLogo from "@/assets/ms-logo.svg";
+import { Link, useNavigate } from "react-router";
+import { useState } from "react";
+import { useLogin } from "../auth.hooks";
+import { Loader } from "lucide-react";
 
-const LoginForm = () => {
-  const { mutateAsync, isPending } = useLogin();
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"form">) {
+  const navigate = useNavigate();
+  const { mutateAsync: login, isPending } = useLogin();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    await mutateAsync({
+    await login({
       email: email,
       password: password,
     });
+    navigate("/dashboard");
   };
 
   return (
-    <Card className="max-w-md mx-auto">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={handleLogin}>
-          <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="email">Email</FieldLabel>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-                value={email}
-                onChange={({ target }) => setEmail(target.value)}
-              />
-            </Field>
-            <Field>
-              <div className="flex items-center">
-                <FieldLabel htmlFor="password">Password</FieldLabel>
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-              <Input
-                disabled={isPending}
-                id="password"
-                type="password"
-                required
-                value={password}
-                onChange={({ target }) => setPassword(target.value)}
-              />
-            </Field>
-            <Field>
-              <Button type="submit">Login</Button>
-              <Button variant="outline" type="button">
-                Login with Google
-              </Button>
-              <FieldDescription className="text-center">
-                Don&apos;t have an account? <a href="#">Sign up</a>
-              </FieldDescription>
-            </Field>
-          </FieldGroup>
-        </form>
-      </CardContent>
-    </Card>
-  );
-};
+    <form
+      autoComplete="off"
+      onSubmit={handleLogin}
+      className={cn("flex flex-col gap-6", className)}
+      {...props}
+    >
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="text-2xl font-bold">Welcome to Classedge</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            A learning Platform of HCCCI
+          </p>
+        </div>
+        <Field>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Input
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            type="email"
+            placeholder="m@example.com"
+            required
+          />
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="password">Password</FieldLabel>
 
-export default LoginForm;
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            type="password"
+            required
+          />
+          <div className="flex items-center">
+            <Link
+              to="/forgot-password"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+        </Field>
+        <Field>
+          <Button type="submit" disabled={isPending}>
+            {isPending ? <Loader className="animate-spin" /> : "Login"}
+          </Button>
+        </Field>
+        <FieldSeparator>or continue with</FieldSeparator>
+        <Field>
+          <Button
+            variant="outline"
+            type="button"
+            onClick={() => navigate("/dashboard")}
+          >
+            <img src={MSLogo} alt="ms-logo" className="size-4" />
+            Microsoft
+          </Button>
+        </Field>
+      </FieldGroup>
+    </form>
+  );
+}
