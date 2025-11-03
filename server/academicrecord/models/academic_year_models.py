a1ea2094a7896d8a9ad42fdcd1f3bde763250ebf
Royd0101 <royd@classify.com.ph>
2025-11-03 16:25:33 +0800

    feat: initialize Django LMS backend with academic record and account models

--- server/academicrecord/models/academic_year_models.py
+++ server/academicrecord/models/academic_year_models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 class AcademicYear(models.Model):
     name = models.CharField(max_length=255)
@@ -9,3 +11,7 @@ class AcademicYear(models.Model):
     
     def __str__(self):
         return self.name
+
+    def clean(self):
+        if self.start_date and self.end_date and self.start_date >= self.end_date:
+            raise ValidationError("Start date must be earlier than end date.")
--- server/teachingload/urls.py
+++ server/teachingload/urls.py
@@ -4,11 +4,11 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router.register(r"subject", SubjectViewSet, basename='subject-view')
-router.register(r"schedule", ScheduleViewSet, basename='schedule-view')
-router.register(r"schedule-type", ScheduleTypeViewSet, basename='schedule-type-view')
+router.register(r"subjects", SubjectViewSet, basename='subject-view')
+router.register(r"schedules", ScheduleViewSet, basename='schedule-view')
+router.register(r"schedule-types", ScheduleTypeViewSet, basename='schedule-type-view')
 router.register(r"target-sdgs", SDGViewSet, basename='target-sdgs-view')
-router.register(r"subject-offering", SubjectOfferingViewSet, basename='subject-offering-view')
+router.register(r"subject-offer", SubjectOfferingViewSet, basename='subject-offer-view')
 
 urlpatterns = [
     path(
