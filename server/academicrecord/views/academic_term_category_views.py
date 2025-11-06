0e3409ee146d69dd8a7f14d17ca766c5674bd112
Royd0101 <royd@classify.com.ph>
2025-11-06 08:04:51 +0800

    feat: initialize Django LMS backend with academic record and accounts modules

--- server/academicrecord/views/academic_term_category_views.py
+++ server/academicrecord/views/academic_term_category_views.py
@@ -3,28 +3,11 @@ from academicrecord.serializers import AcademicTermCategorySerializer
 from rest_framework.authentication import SessionAuthentication
 from accounts.utils import CookieJWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-from common.pagination import CustomPagination
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
 
 class AcademicTermCategoryViewSet(ModelViewSet):
     serializer_class = AcademicTermCategorySerializer
     authentication_classes = [SessionAuthentication, CookieJWTAuthentication]
     permission_classes = [IsAuthenticated]
-    pagination_class = CustomPagination
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ['name']
-    search_fields = ['name']
-    ordering_fields = ['name']
     
     def get_queryset(self):
         return self.serializer_class.Meta.model.objects.all()
-
-    # Non-paginated endpoint: /api/academic-term-categories/all/
-    @action(detail=False, methods=['get'], url_path='all')
-    def list_all(self, request):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
